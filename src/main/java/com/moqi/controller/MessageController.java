5d721f1838cd1eb3353b7d26507d61c417a1ae29
moqimoqidea <moqimoqidea@gmail.com>
2025-02-14 16:31:40 +0800

    Add logging and update message endpoints in MessageController

--- src/main/java/com/moqi/controller/MessageController.java
+++ src/main/java/com/moqi/controller/MessageController.java
@@ -1,19 +1,27 @@
 package com.moqi.controller;
 
 import com.moqi.model.Message;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/messages")
 public class MessageController {
 
-    @GetMapping("/{content}")
-    public Message getMessage(@PathVariable String content) {
-        return new Message(content);
+    @GetMapping("/hello")
+    public Message getHello() {
+        log.info("Received GET request to /api/messages/hello");
+        Message response = new Message("hello");
+        log.info("Returning response: {}", response);
+        return response;
     }
 
     @PostMapping
     public Message createMessage(@RequestBody Message message) {
-        return message;
+        log.info("Received POST request to /api/messages with body: {}", message);
+        Message response = new Message(message.getContent());
+        log.info("Returning response: {}", response);
+        return response;
     }
 } 
